b331c0c48b961f65cff76c8076a22c88e99a65bc
Jiří Suchomel <jsuchome@suse.cz>
2008-05-16 13:26:15 +0000

    updated templates (used coolo's LiveCD's config)
    svn path=/trunk/product-creator/; revision=47615

--- src/data/kiwi_templates/iso/config.sh
+++ src/data/kiwi_templates/iso/config.sh
@@ -3,35 +3,19 @@
 # FILE          : config.sh
 #----------------
 # PROJECT       : OpenSuSE KIWI Image System
-# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
+# COPYRIGHT     : (c) 2006,2007,2008 SUSE LINUX Products GmbH. All rights reserved
 #               :
-# AUTHOR        : Marcus Schaefer <ms@suse.de>
+# AUTHOR        : Marcus Schaefer <ms@suse.de>, Stephan Kulow <coolo@suse.de>
 #               :
-# BELONGS TO    : Operating System images
-#               :
-# DESCRIPTION   : configuration script for SUSE based
-#               : operating systems
-#               :
-#               :
-# STATUS        : BETA
-#----------------
+# LICENSE       : BSD
 #======================================
 # Functions...
 #--------------------------------------
 test -f /.kconfig && . /.kconfig
 test -f /.profile && . /.profile
 
-#======================================
-# Sound?
-#--------------------------------------
-
-# Load sound drivers by default
-perl -ni -e 'm,^blacklist snd-, || print;' \
-	/etc/modprobe.d/blacklist
-
-# and unmute their mixers.
-perl -pi -e 's,/sbin/alsactl -F restore,/bin/set_default_volume -f,;' \
-	/etc/udev/rules.d/40-alsa.rules
+exec > /var/log/config.log
+exec 2>&1
 
 #======================================
 # Greeting...
@@ -39,58 +23,29 @@ perl -pi -e 's,/sbin/alsactl -F restore,/bin/set_default_volume -f,;' \
 echo "Configure image: [$name]..."
 
 #======================================
-# Activate services
+# Load sound drivers by default
 #--------------------------------------
-suseActivateServices
+perl -ni -e 'm,^blacklist snd-, || print;' \
+	/etc/modprobe.d/blacklist
 
-#======================================
+# and unmute their mixers.
+perl -pi -e 's,/sbin/alsactl -F restore,/bin/set_default_volume -f,;' \
+	/etc/udev/rules.d/40-alsa.rules
+
 # Deactivate services
 #--------------------------------------
-suseRemoveService boot.multipath
 suseRemoveService boot.device-mapper
 suseRemoveService mdadmd
-suseRemoveService multipathd
-suseRemoveService rpmconfigcheck
-suseRemoveService waitfornm
-suseRemoveService smb
-suseRemoveService xfs
-suseRemoveService nmb
-suseRemoveService autofs
 suseRemoveService rpasswdd
 suseRemoveService boot.scsidev
 suseRemoveService boot.md
-suseInsertService create_xconf
-suseService boot.rootfsck off
+
 # these two we want to disable for policy reasons
 chkconfig sshd off
 chkconfig cron off
 
-# these are disabled because kiwi enables them without being default
-chkconfig aaeventd off
-chkconfig autoyast off
-chkconfig boot.sched off
-chkconfig create_xconf off
-chkconfig dvb off
-chkconfig esound off
-chkconfig fam off
-chkconfig festival off
-chkconfig hotkey-setup off
-chkconfig ipxmount off
-chkconfig irda off
-chkconfig java.binfmt_misc off
-chkconfig joystick off
-chkconfig lirc off
-chkconfig lm_sensors off
-chkconfig nfs off
-chkconfig ntp off
-chkconfig openct off
-chkconfig pcscd off
-chkconfig powerd off
-chkconfig raw off
-chkconfig saslauthd off
-chkconfig spamd off
-chkconfig xinetd off
-chkconfig ypbind off
+# enable create_xconf
+chkconfig create_xconf on
 
 cd /
 patch -p0 < /tmp/config.patch
@@ -98,24 +53,38 @@ rm /tmp/config.patch
 
 insserv 
 
-rpm -e smart
-rpm -e rpm-python
-rpm -e python
-
-: > /var/log/zypper.log
 rm -rf /var/cache/zypp/raw/*
 
-zypper addrepo http://download.opensuse.org/distribution/10.3/repo/oss/ 10.3
-zypper addrepo http://download.opensuse.org/update/10.3/ 10.3-update
+zypper addrepo -d http://download.opensuse.org/distribution/SL-OSS-factory/inst-source/ factory-oss
+zypper addrepo -d http://download.opensuse.org/distribution/SL-Factory-non-oss/inst-source-extra/ factory-non-oss
+zypper addrepo -d http://download.opensuse.org/update/11.0/ updates
+
+#======================================
+# /etc/sudoers hack to fix #297695 
+# (Installation Live CD: no need to ask for password of root)
+#--------------------------------------
+sed -e "s/ALL ALL=(ALL) ALL/ALL ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers > /tmp/sudoers && mv /tmp/sudoers /etc/sudoers
+chmod 0440 /etc/sudoers
+
+# empty password is ok
+pam-config -a --nullok
+
+: > /var/log/zypper.log
 
 #======================================
 # SuSEconfig
 #--------------------------------------
+mount -o bind /lib/udev/devices /dev
 suseConfig
+umount /dev
+
+test -x /usr/bin/kbuildsycoca4 && su - linux -c /usr/bin/kbuildsycoca4
 
 #======================================
 # Umount kernel filesystems
 #--------------------------------------
 baseCleanMount
+
+rm -rf /var/lib/smart
 
 exit 0
